fa08c42b9554bac9d650276adfddf59f0c5c1ccf
Ian Hollister <ian.hollister@dbresults.com.au>
2017-08-24 16:11:02 +1000

    Added a comment

--- server.js
+++ server.js
@@ -349,6 +349,8 @@ var Chat;
             }
             this.internallistener = server.listen(this.nodePort, this.localServer);
 
+			console.log("Listening on " + localServer + ", server_port " + nodePort);
+			
             // Socket Communications
             this.listener = Utils.IO.listen(server, this.localServer);
             this.InitChatCommunications();
